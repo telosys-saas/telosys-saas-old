2e902d8cce6b8cc8205808be1ed533f1012e0b21
Ludovic Chaboud <l.chaboud@gmail.com>
2015-12-30 19:20:42 +0100

    pages

--- src/main/resources/META-INF/web/app/view/index/welcome/welcome.view.js
+++ src/main/resources/META-INF/web/app/view/index/welcome/welcome.view.js
@@ -8,15 +8,13 @@ var Welcome = {
     $.get('app/view/index/welcome/welcome.html', function(html) {
       $('#main').html(html);
       $('#main .parallax').parallax();
-      $('#login').html(Login.getDisplayHtml());
       $('footer').hide();
 
-      var options = [{
-        selector: '#workflow-step1',
-        offset: 400,
-        callback: 'Materialize.showStaggeredList("workflow-step1")'
-      }];
-      Materialize.scrollFire(options);
+      $.get('app/view/index/login/login.html', function (html) {
+        $('#login').html(html);
+        $('#main').click();
+      });
+
     });
   }
 
