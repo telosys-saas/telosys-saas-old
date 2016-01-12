7ff8604b8985d52c16a1852d6a1063518041e6b6
ludo1026 <ludo1026@free.fr>
2016-01-12 18:34:05 +0100

    pac4j

--- src/main/java/org/telosys/saas/security/Pac4jConfigFactory.java
+++ src/main/java/org/telosys/saas/security/Pac4jConfigFactory.java
@@ -14,10 +14,10 @@ public class Pac4jConfigFactory implements ConfigFactory {
         gitHubClient.setScope("user:email");
         
         // FormClient formClient = new FormClient("http://localhost:8080/login.html", new SimpleTestUsernamePasswordAuthenticator());
-        FormClient formClient = new FormClient("http://localhost:8080", new FormAuthenticator());
+        FormClient formClient = new FormClient("/", new FormAuthenticator());
         
         Clients clients =
-                new Clients("http://localhost:8080/api/callback",
+                new Clients("/api/callback",
                 	formClient,
                     gitHubClient
                 );
