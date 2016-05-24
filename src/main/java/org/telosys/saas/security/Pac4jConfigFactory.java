13387a2a6788c2844d6a489165060dbceb148808
Laurent Guerin <laguerin@LFR002634.corp.capgemini.com>
2016-05-24 19:15:14 +0200

    Useless import removed

--- src/main/java/org/telosys/saas/security/Pac4jConfigFactory.java
+++ src/main/java/org/telosys/saas/security/Pac4jConfigFactory.java
@@ -8,7 +8,6 @@ import org.pac4j.core.client.Clients;
 import org.pac4j.core.config.Config;
 import org.pac4j.core.config.ConfigFactory;
 import org.pac4j.http.client.indirect.FormClient;
-import org.pac4j.http.credentials.authenticator.test.SimpleTestUsernamePasswordAuthenticator;
 import org.pac4j.oauth.client.GitHubClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
